6930b5a63a8fd104d8c21dd35cb446cacdc5864a
Leandro Caballaro <leandro@example.com>
2026-05-15 16:03:00 -0300

    Re-upload project

---
+++ funkopop/Project/Api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import *
+from django.contrib.auth.decorators import permission_required
+
+# Create your views here.
+def Home(request):
+    return render (request,'Base.html')
+@permission_required('Api.add_producto')
+
+def NuevoProductos(request):
+    data={
+        'Formulario': FormularioProductos()
+    }
+    if request.method == 'POST':
+        formulario = FormularioProductos(data=request.POST,files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            data['Mensaje'] = "Producto Guardado Correctamente"
+        else:
+            data['Mensaje'] = "Error al Guardar el Producto"
+    return render(request, 'Pages/NuevoProducto.html', data)
